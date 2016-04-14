3b11773df0f070ae52b813c0e132ccee04af4132
jsruesch <jake@gofarwell.com>
2016-04-14 16:09:30 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML >
+<html lang="en">
+<head>
+    
+    <title>jweb</title>
+    <meta name="description"
+          content="">
+        
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="stylesheets/style.min.css"/>
+    
+    <!-- Responsive Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="stylesheets/responsive_styles.min.css"/>
+    
+    <!-- Mobile Nav Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="stylesheets/mobile_nav_style.min.css"/>
+    <script type="text/javascript" src="js/modernizr.mobilenav.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <!-- jQuery -->
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+    <!-- Fontello -->
+    <link rel="stylesheet" href="stylesheets/fontello/fontello.css">
+    <link rel="stylesheet" href="stylesheets/fontello/animation.css"><!--[if IE 7]>
+    <link rel="stylesheet" href="stylesheets/fontello/fontello-ie7.css"><![endif]-->
+    <script>
+        function toggleCodes(on) {
+          var obj = document.getElementById('icons');
+          
+          if (on) {
+            obj.className += ' codesOn';
+          } else {
+            obj.className = obj.className.replace(' codesOn', '');
+          }
+        }
+    </script>
+    
+</head>
+
+<body id="">
---
+++ navigation.php
@@ -0,0 +1,10 @@
+<!-- Mobile Navitgation -->
+<nav  id="menu" class="mobile_nav">
+    <ul>
+        <a href="#"><li>Link 1</li></a>
+        <a href="#"><li>Link 2</li></a>
+        <a href="#"><li>Link 3</li></a>
+        <a href="#"><li>Link 4</li></a>
+        <a href="#"><li>Link 5</li></a>
+    </ul>
+</nav>
