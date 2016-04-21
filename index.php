30d53339ea43b28c1aa97d8231f8e46532be47b1
jsruesch <jake@gofarwell.com>
2016-04-20 22:05:54 -0500

    fix team list grid issues

--- index.php
+++ index.php
@@ -14,11 +14,7 @@
 
 <div class="container-fluid mc">
     <div class="container">
-        <div class="col-sm-12 bg-gold">
-            <h1 class="text-center text-grey">June 10-12</h1>
-        </div>
-        
-        <div class="full bg-grey team-list">
+        <div class="full team-list">
             <div class="col-sm-3 bg-grey"><p class="text-gold">Teams</p></div>
             <div class="col-sm-3 bg-blue"><p>Team Blue</p></div>
             <div class="col-sm-3 bg-grey"><p>Team Black</p></div>
@@ -31,7 +27,7 @@
         </div>
         
         <!-- schedule-overview -->
-        <div class="schedule-overview">
+        <div class="full schedule-overview">
             <div class="col-sm-4 bg-grey">
                 <div class="team-schedule"><?php require_once('schedules/friday.php') ?></div>
             </div>
