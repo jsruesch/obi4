2f65007ab152498ba83222e0dce6a50a2b17c4bb
jsruesch <jake@gofarwell.com>
2016-04-21 19:35:53 -0500

    add OBI logo to nav and mobile nav

--- inc/navigation.php
+++ inc/navigation.php
@@ -11,12 +11,14 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
+                    <div class="obi-logo-mobile"></div>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.php">Home</a></li>
+                        <li><div class="obi-logo"></div></li>
+                        <li><a href="index.php" class="pleft">Home</a></li>
                         
                         <!-- START: Teams -->
                         <li class="dropdown">
