5d351faac7b29304cc14af5eceedcebd05298e20
jsruesch <jake@gofarwell.com>
2016-05-18 20:36:06 -0500

    change skater price

--- inc/footer.php
+++ inc/footer.php
@@ -6,7 +6,7 @@
         </div>
         <div class="col-sm-4">
             <ul>
-                <li>Skater Cost: <strong class="text-gold">$75</strong></li>
+                <li>Skater Cost: <strong class="text-gold">$50</strong></li>
                 <li>Goalie Cost: <strong class="text-gold">$25</strong></li>
                 <li>Each team will get <span class="text-gold">six, two-period games</span> in <span class="text-gold">Round Robin format</span>. Periods will be 20 minutes each.</li>
             </ul>
--- schedules/friday.php
+++ schedules/friday.php
@@ -6,7 +6,7 @@
             <td class="game-time">0:00 pm <br /> <span class="rink">Cardinal Rink</span></td>
             <td class="opponent">
                 <ul>
-                    <li class="text-red">Team 1 Name</li>
+                    <li class="text-red">shmageggy</li>
                     <li class="rink">vs</li>
                     <li class="text-yellow">Team 2 Name</li>
                 </ul>
