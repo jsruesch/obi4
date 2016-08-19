1a9ecd0003838dac5a75d4627e2f7d0fa3ce7c97
jsruesch <jake@gofarwell.com>
2016-08-19 16:31:23 -0500

    Revise points and standings for team pages

--- team-blue.php
+++ team-blue.php
@@ -16,21 +16,23 @@
 <div class="container-fluid">
     <div class="container">
 
+        <!-- Points and Standings -->
+        <div class="col-sm-12 points-and-standings">
+          <div class="col-sm-6 points">
+            <h3>Points: 00</h3>
+          </div>
+          <div class="col-sm-6 standings">
+            <h3>Standing: 1</h3>
+          </div>
+        </div>
+
         <!-- Schedule -->
-        <div class="col-sm-6 bg-grey mc">
+        <div class="col-sm-6 bg-grey">
             <div class="team-schedule"><?php require_once('schedules/schedule-blue.php'); ?></div>
         </div>
 
         <!-- Roster and Players -->
-        <div class="col-sm-6 roster bg-grey mc">
-            <div class="col-sm-12 team-current">
-                <div class="col-sm-6 bg-gold">
-                    <h3 class="text-grey text-left">Points: 00</h3>
-                </div>
-                <div class="col-sm-6 bg-gold">
-                    <h3 class="text-grey text-right">Standing: 1</h3>
-                </div>
-            </div>
+        <div class="col-sm-6 roster bg-grey">
             <div class="col-sm-12 team-roster"><?php require_once('rosters/roster-blue.php'); ?></div>
         </div>
 
