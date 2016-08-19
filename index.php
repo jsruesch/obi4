cf40858c7bd6f6d457df6ed105c65f91ea927cac
jsruesch <jake@gofarwell.com>
2016-08-18 20:34:21 -0500

    Add standings to index

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 
 <div class="container-fluid mc">
     <div class="container">
+        <!--
         <div class="full team-list">
             <div class="col-sm-3 bg-grey"><p class="text-gold">Teams</p></div>
             <a href="team-blue.php"><div class="col-sm-3 bg-blue"><p>                           <?php include('rosters/team-names.php'); echo $teamBlueFull; ?>  </p></div></a>
@@ -33,20 +34,82 @@
             <a href="team-yellow.php"><div class="col-sm-3 bg-yellow"><p class="text-grey">     <?php include('rosters/team-names.php'); echo $teamYellowFull; ?></p></div></a>
             <a href="team-tan.php"><div class="col-sm-3 bg-tan"><p class="text-grey">           <?php include('rosters/team-names.php'); echo $teamTanFull; ?></p></div></a>
             <a href="team-light-blue.php"><div class="col-sm-3 bg-light-blue"><p class="padbot"><?php include('rosters/team-names.php'); echo $teamLightBlueFull; ?></p></div></a>
+        </div>-->
+        <div class="col-sm-4 bg-grey">
+            <div class="full">
+                <h3 class="text-white">Standings</h3>
+            </div>
+            <div class="full standings">
+                <table class="full">
+                    <tr>
+                        <td class="standing-number"><span class="text-gold">1</span></td>
+                        <td class="standing-team"><div class="bg-blue"><span class="text-white">
+                            <?php include('rosters/team-names.php'); echo $teamBlueFull; ?></span></div>
+                        </td>
+                        <td class="standing-points"><span class="text-gold">00 pts</span></td>
+                    </tr>
+                    <tr>
+                        <td class="standing-number">2</td>
+                        <td class="standing-team"><div class="bg-grey"><span class="text-white">
+                            <?php include('rosters/team-names.php'); echo $teamBlackFull; ?></span></div>
+                        </td>
+                        <td class="standing-points">00 pts</td>
+                    </tr>
+                    <tr>
+                        <td class="standing-number">3</td>
+                        <td class="standing-team"><div class="bg-red"><span class="text-white">
+                            <?php include('rosters/team-names.php'); echo $teamRedFull; ?></span></div>
+                        </td>
+                        <td class="standing-points">00 pts</td>
+                    </tr>
+                    <tr>
+                        <td class="standing-number">4</td>
+                        <td class="standing-team"><div class="bg-white"><span class="text-grey">
+                            <?php include('rosters/team-names.php'); echo $teamGreyFull; ?></span></div>
+                        </td>
+                        <td class="standing-points">00 pts</td>
+                    </tr>
+                    <tr>
+                        <td class="standing-number">5</td>
+                        <td class="standing-team"><div class="bg-yellow"><span class="text-grey">
+                            <?php include('rosters/team-names.php'); echo $teamYellowFull; ?></span></div>
+                        </td>
+                        <td class="standing-points">00 pts</td>
+                    </tr>
+                    <tr>
+                        <td class="standing-number">6</td>
+                        <td class="standing-team"><div class="bg-tan"><span class="text-grey">
+                            <?php include('rosters/team-names.php'); echo $teamTanFull; ?></span></div>
+                        </td>
+                        <td class="standing-points">00 pts</td>
+                    </tr>
+                    <tr>
+                        <td class="standing-number">7</td>
+                        <td class="standing-team"><div class="bg-light-blue"><span class="text-white">
+                            <?php include('rosters/team-names.php'); echo $teamLightBlueFull; ?></span></div>
+                        </td>
+                        <td class="standing-points">00 pts</td>
+                    </tr>
+                </table>
+            </div>
         </div>
-
-        <!-- schedule-overview -->
-        <div class="full schedule-overview">
-            <!--<div class="col-sm-4 bg-grey">
-                <div class="team-schedule"><?php require_once('schedules/friday.php') ?></div>
-            </div>-->
-            <div class="col-sm-6 bg-grey">
-                <div class="team-schedule"><?php require_once('schedules/saturday.php') ?></div>
+        <div class="col-sm-8 bg-grey">
+            <div class="full">
+                <h3 class="text-white">Schedule</h3>
             </div>
-            <div class="col-sm-6 bg-grey">
-                <div class="team-schedule"><?php require_once('schedules/sunday.php') ?></div>
-            </div>
-        </div><!-- /.schedule-overview -->
+            <!-- schedule-overview -->
+            <div class="full schedule-overview">
+                <!--<div class="col-sm-4 bg-grey">
+                    <div class="team-schedule"><?php require_once('schedules/friday.php') ?></div>
+                </div>-->
+                <div class="col-sm-12">
+                    <div class="team-schedule"><?php require_once('schedules/saturday.php') ?></div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="team-schedule"><?php require_once('schedules/sunday.php') ?></div>
+                </div>
+            </div><!-- /.schedule-overview -->
+        </div>
     </div>
 </div>
 <!-- END: Main Contents -->
